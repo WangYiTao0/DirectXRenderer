9b8e34fa0c9ac81d251bf4b94052662a287d13e2
W <as5818956@gmail.com>
2020-07-07 14:12:22 +0900

    all models casting shadows

--- Sandbox/asset/shader/ShadowTest_VS.hlsl
+++ Sandbox/asset/shader/ShadowTest_VS.hlsl
@@ -1,15 +1,13 @@
 #include "Common/Transform.hlsli"
-cbuffer ShadowTransform
-{
-    matrix shadowView;
-};
+#include "Common/VShadow.hlsli"
+
 
 struct VSOut
 {
     float3 viewPos : Position;
     float3 viewNormal : Normal;
     float2 tc : Texcoord;
-    float4 shadowCamPos : ShadowPosition;
+    float4 shadowHomoPos : ShadowPosition;
     float4 pos : SV_Position;
 };
 
@@ -19,11 +17,7 @@ VSOut main(float3 pos : Position, float3 n : Normal, float2 tc : Texcoord)
     vso.viewPos = (float3) mul(float4(pos, 1.0f), modelView);
     vso.viewNormal = mul(n, (float3x3) modelView);
     vso.pos = mul(float4(pos, 1.0f), modelViewProj);
-    vso.tc = tc;
-    const float4 shadowCamera = mul(float4(pos, 1.0f), model); //world pos
-    const float4 shadowHomo = mul(shadowCamera, shadowView); // camera view space
-    
-    //NDC space
-    vso.shadowCamPos = shadowHomo * float4(0.5f, -0.5f, 1.0f, 1.0f) + (float4(0.5f, 0.5f, 0.0f, 0.0f) * shadowHomo.w);
+    vso.tc = tc;    
+    vso.shadowHomoPos = ToShadowHomoSpace(pos, model);
     return vso;
 }
