03a7e4e3c6ceb570473aa5de50d0d43733d32ded
W <as5818956@gmail.com>
2020-10-18 15:48:04 +0900

    Reduce scene Object

--- Sandbox/src/Scenes/Scene3D.cpp
+++ Sandbox/src/Scenes/Scene3D.cpp
@@ -16,26 +16,26 @@ Scene3D::Scene3D(dr::Win32Window& wnd)
 	cameras.AddCamera(std::make_unique<dr::Camera3D>(wnd.Gfx(), "B", dx::XMFLOAT3{ -13.5f,28.8f,-6.4f }, dr::PI / 180.0f * 13.0f, dr::PI / 180.0f * 61.0f));
 	cameras.AddCamera(light.ShareCamera());
 
-	cube.SetPos({ 10.0f,5.0f,6.0f });
-	cube2.SetPos({ 10.0f,5.0f,14.0f });
+	//cube.SetPos({ 10.0f,5.0f,6.0f });
+	//cube2.SetPos({ 10.0f,5.0f,14.0f });
 
-	nano.SetRootTransform(
-		dx::XMMatrixRotationY(dr::PI / 2.f) *
-		dx::XMMatrixTranslation(27.f, -0.56f, 1.7f)
+	nano.SetRootTransform(dx::XMMatrixRotationY(dr::PI / 2.f) *
+		dx::XMMatrixTranslation(27.f, -0.56f, 1.7f) * 
+		dx::XMMatrixScaling(0.3f, 0.3f, 0.3f)
 	);
-	gobber.SetRootTransform(
-		dx::XMMatrixRotationY(-dr::PI / 2.f) *
-		dx::XMMatrixTranslation(-8.f, 10.f, 0.f)
-	);
+	//gobber.SetRootTransform(
+	//	dx::XMMatrixRotationY(-dr::PI / 2.f) *
+	//	dx::XMMatrixTranslation(-8.f, 10.f, 0.f)
+	//);
 
 
-	cube.LinkTechniques(rg);
-	cube2.LinkTechniques(rg);
 	light.LinkTechniques(rg);
 	sponza.LinkTechniques(rg);
-	gobber.LinkTechniques(rg);
-	nano.LinkTechniques(rg);
 	cameras.LinkTechniques(rg);
+	nano.LinkTechniques(rg);
+	//cube.LinkTechniques(rg);
+	//cube2.LinkTechniques(rg);
+	//gobber.LinkTechniques(rg);
 
 	rg.BindShadowCamera(*light.ShareCamera());
 }
@@ -59,19 +59,20 @@ void Scene3D::Draw(float dt)
 	using namespace dr;
 
 	light.Submit(dr::Chan::main);
-	cube.Submit(dr::Chan::main);
+	cameras.Submit(dr::Chan::main);
 	sponza.Submit(dr::Chan::main);
+	nano.Submit(dr::Chan::main);
+	
+	/*cube.Submit(dr::Chan::main);
 	cube2.Submit(dr::Chan::main);
 	gobber.Submit(dr::Chan::main);
-	nano.Submit(dr::Chan::main);
-	cameras.Submit(dr::Chan::main);
+	*/
 
 	sponza.Submit(Chan::shadow);
-	cube.Submit(Chan::shadow);
-	sponza.Submit(Chan::shadow);
-	cube2.Submit(Chan::shadow);
-	gobber.Submit(Chan::shadow);
 	nano.Submit(Chan::shadow);
+	//cube.Submit(Chan::shadow);
+	//cube2.Submit(Chan::shadow);
+	//gobber.Submit(Chan::shadow);
 
 	rg.Execute(wnd.Gfx());
 
@@ -94,14 +95,16 @@ void Scene3D::SpawnImguiWindow()
 	static dr::MP nanoProbe{ "Nano" };
 
 	// imgui window to control camera
-	sponzeProbe.SpawnWindow(sponza);
-	gobberProbe.SpawnWindow(gobber);
-	nanoProbe.SpawnWindow(nano);
-
 	cameras.SpawnWindow(wnd.Gfx());
 	light.SpawnControlWindow();
+	sponzeProbe.SpawnWindow(sponza);
+	nanoProbe.SpawnWindow(nano);
+	
+	/*
+	gobberProbe.SpawnWindow(gobber);
 	cube.SpawnControlWindow(wnd.Gfx(), "Cube 1");
 	cube2.SpawnControlWindow(wnd.Gfx(), "Cube 2");
+	*/
 	rg.RenderWindows(wnd.Gfx());
 
 	rg.Reset();
